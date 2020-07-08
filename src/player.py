9d91d8b67a79233e9b59ffa885d8fcbe79437acb
seanmx96 <seanmx96@users.noreply.github.com>
2020-07-08 15:52:53 -0500

    Created a Player class with constructor and string methods

--- src/player.py
+++ src/player.py
@@ -1,2 +1,14 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+# create a Player class, define constructor and string methods
+class Player:
+    def __init__(self, name, health, weapon, armor, current_room):
+        self.name = name
+        self.health = health
+        self.weapon = weapon
+        self.armor = armor
+        self.current_room = current_room
+
+    def __str__(self):
+        return f'Player: {self.name}, {self.health}, {self.weapon}, {self.armor}, {self.current_room}'
